9705497027cbf7ee2a09431b495f57fcf9deb376
Mirek Nguyen <nguyemir@fit.cvut.cz>
2021-12-29 00:03:39 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<title>Mirek Nguyen</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+
+<body>
+   <h1>Welcome to personal my website!</h1>
+   <div>
+      <p>Older projects:</p>
+      <p><a href="koreanist">Koreanist</a></p>
+      <p><a href="kalendar">Advent Calendar</a></p>
+   </div>
+   <?php echo "<p>hello</p>"; ?>
+</body>
+
+</html>
+
+</html>
+
+</html>
