644b8aeb81d6c59cddbbae0bc269d96ad1b4f0bb
Mirek Nguyen <nguyemir@fit.cvut.cz>
2022-12-31 03:14:35 +0100

    feat: mobile responsive navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,20 +1,29 @@
 import Link from "next/link";
 import { useTheme } from "next-themes";
+import { useState } from "react";
 const Navbar = () => {
   function switchTheme() {
     theme === "dark" ? setTheme("light") : setTheme("dark");
   }
   const { theme, setTheme } = useTheme();
+  const [menu, setMenu] = useState(false);
+  function toggleMenu() {
+    menu === false ? setMenu(true) : setMenu(false);
+  }
   return (
-    <nav className="h-16 px-5 sm:px-14 bg-transparent dark:text-slate-100 w-screen font-montserrat">
-      <ul className="flex justify-center items-center text-xl w-full h-full">
-        <li className="mr-auto text-2xl">
+    <nav
+      className={`px-5 sm:px-14 bg-transparent dark:text-slate-100 w-screen font-montserrat ${
+        menu === false ? "h-auto" : "h-auto"
+      }`}
+    >
+      <ul className="flex justify-center items-center text-xl w-full h-16">
+        <li className="mr-auto text-xl sm:text-2xl">
           <Link href="/">Mirek Nguyen</Link>
         </li>
-        <li className="ml-5 invisible sm:visible">
+        <li className="ml-5 hidden sm:block">
           <Link href="/">Home</Link>
         </li>
-        <li className="ml-5 invisible sm:visible">
+        <li className="ml-5 hidden sm:block">
           <Link href="/contact">Contact</Link>
         </li>
         <li
@@ -28,7 +37,28 @@ const Navbar = () => {
             alt={theme === "light" ? "light-mode-icon" : "dark-mode-icon"}
           ></img>
         </li>
-        <li className="ml-5 sm:invisible sm:hidden">menu</li>
+        <li
+          className="ml-5 sm:invisible sm:hidden hover:cursor-pointer"
+          onClick={toggleMenu}
+        >
+          <div>
+            <span className="block w-6 h-1 my-1 bg-black dark:bg-white"></span>
+            <span className="block w-6 h-1 my-1 bg-black dark:bg-white"></span>
+            <span className="block w-6 h-1 my-1 bg-black dark:bg-white"></span>
+          </div>
+        </li>
+      </ul>
+      <ul
+        className={`flex justify-center items-center flex-col ${
+          menu === false ? "hidden" : "grid"
+        }`}
+      >
+        <li className="my-10 mx-auto" onClick={toggleMenu}>
+          <Link href="/">Home</Link>
+        </li>
+        <li className="my-10 mx-auto" onClick={toggleMenu}>
+          <Link href="/contact">Contact</Link>
+        </li>
       </ul>
     </nav>
   );
