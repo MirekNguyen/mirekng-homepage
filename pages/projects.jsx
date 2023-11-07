12202779919a4c553b4c91166f14fe94c545d3b7
Mirek Nguyen <mireknguyenbinh@seznam.cz>
2023-11-07 12:19:58 +0100

    feat: add new project

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -7,6 +7,7 @@ import {
   FaGamepad,
   FaGlobeEurope,
   FaClipboardList,
+  FaFileCode
 } from "react-icons/fa";
 import { CgFileDocument } from "react-icons/cg";
 import {
@@ -18,12 +19,18 @@ import {
   SiCsharp,
   SiPhp,
   SiPostgresql,
-  SiJavascript,
+  SiSymfony,
   SiDocker,
   SiGnubash,
   SiLinux,
   SiFedora,
 } from "react-icons/si";
+import {
+  TbApi
+} from "react-icons/tb"
+import {
+  BiSolidFileJson
+} from "react-icons/bi"
 const projects = () => {
   return (
     <AnimatedPage>
@@ -68,7 +75,7 @@ const projects = () => {
         <h1 id="dungeon" className="flex place-items-center text-3xl pb-8 pt-4">
           Dungeon game
         </h1>
-                  <div className="w-screen h-auto flex flex-col md:flex-row items-center justify-center">
+        <div className="w-screen h-auto flex flex-col md:flex-row items-center justify-center">
           <Image
             src="/images/dungeon.jpg"
             alt="Dungeon preview"
@@ -141,7 +148,7 @@ const projects = () => {
           <SiFedora title="Fedora" size="32" />
         </div>
         <div className="flex flex-col md:flex-row gap-4 mb-8">
-          <Link href="https://dashboard.mirekng.com">
+          <Link href="https://dashboard.mirekng.net">
             <button className="flex place-items-center text-2xl sm:text-2xl text-white bg-red-400 rounded px-5 py-2 dark:border-white border-gray-800 border-2">
               Dashboard
               <FaClipboardList className="ml-2 inline" />
@@ -155,33 +162,50 @@ const projects = () => {
           </Link>
         </div>
         <hr className="w-screen max-w-2xl h-px bg-black dark:bg-white border-0 my-8" />
-        <h1 id="vietnamist" className="text-3xl pb-8 pt-4">
-          Vietnamist
+        <h1 id="symfony-api" className="text-3xl pb-8 pt-4">
+          Symfony Dummy API
         </h1>
-        <div className="w-screen h-auto flex items-center justify-center">
+        <div className="w-screen h-auto flex flex-col md:flex-row md:gap-2 items-center justify-center">
           <Image
-            src="/images/vietnamist.jpg"
-            alt="Vietnamist preview"
-            width="412px"
-            height="256px"
+            src="/images/symfony-api-schema.jpg"
+            alt="Symfony API preview"
+            width="280px"
+            height="257px"
+            fill
+          />
+          <Image
+            src="/images/schema.jpg"
+            alt="Symfony API schema"
+            width={453}
+            height={256}
             fill
           />
         </div>
         <blockquote className="mx-20 max-w-2xl pt-8 pb-4">
-          Vietnamist is a web-based application designed to help users learn
-          Vietnamese vocabulary in the context of sentences. It is is built
-          using PHP and PostgresSQL and uses AJAX to load data asynchronously,
-          making the user experience seamless and responsive.
+          A sample book lending backend that follows the REST
+          API guidelines and includes all the basic CRUD operations. It was
+          developed as a practical implementation of the database modeling
+          concepts I learned in a university course. Every midnight, the API
+          automatically regenerates its dataset to ensure that data is always
+          available for testing and demonstration purposes.
         </blockquote>
         <div className="flex items-center gap-4 pb-8">
           <p className="inline">Technologies:</p>
+          <SiSymfony title="Symfony" size="32" />
           <SiPhp title="PHP" size="32" />
+          <SiDocker title="Docker" size="32" />
           <SiPostgresql title="Postgresql" size="32" />
-          <SiJavascript title="Javascript" size="32" />
+          <TbApi title="API" size="32" />
         </div>
 
         <div className="flex gap-4 pb-8 mb-8">
-          <Link href="https://github.com/MirekNguyen/vietnamist">
+          <Link href="https://api.mirekng.net">
+            <button className="flex place-items-center text-2xl sm:text-2xl text-white bg-red-400 rounded px-5 py-2 dark:border-white border-gray-800 border-2">
+              Try API
+              <FaFileCode className="ml-2 inline" />
+            </button>
+          </Link>
+          <Link href="https://github.com/MirekNguyen/symfony-api">
             <button className="flex place-items-center text-2xl sm:text-2xl text-white dark:bg-transparent bg-gray-800 rounded px-5 py-2 dark:border-white border-gray-800 border-2">
               Github
               <FaGithub className="ml-2 inline" />
